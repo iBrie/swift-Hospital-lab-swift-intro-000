02bf2b6a0806a1472d1d491ff494470f9ab189b4
iBrie <ibrigitte@me.com>
2017-07-20 02:25:18 +0200

    Solved but not correct

--- Hospital/School.swift
+++ Hospital/School.swift
@@ -5,14 +5,86 @@
 
 
 
+protocol Payable {
+  func wages() -> Double
+}
 
+extension Payable {
+  func wages() -> Double {
+    return 50_000.0
+  }
+}
 
+protocol TimeOff {
+  var vacationDays : Int {get set}
+  func requestForVacation(days : Int) -> Bool
+  }
 
+extension TimeOff {
+  func requestForVacation(days : Int) -> Bool {
+    if days > vacationDays{
+    return false
+  }
+    else{
+      return true
+    }
+  }
+}
+enum Punishment {
+  case severe,
+        light,
+        medium
+}
+protocol Reprimand {
+   func disciplineStudent(scale:Punishment) -> String
+  func callParentDeliveringMessage(scale: Punishment) -> String
+}
 
+extension Reprimand {
+  func disciplineStudent(scale:Punishment) -> String{
+    switch scale {
+    case .severe:
+      return "We are expelling you from the school!"
+    case .light:
+      return "You're to go back to class, don't do it again."
+    case .medium:
+      return "Why would you think that's a good idea?! You're going to detention."
+    }
+  }
+  
+  
+  func callParentDeliveringMessage(scale: Punishment) -> String{
+    switch scale {
+    case .severe:
+      return "Your child has been expelled from the school."
+    case .light:
+      return "Your child thought it was funny to put gum in Amy's hair."
+    case .medium:
+      return "Your child has become a terror in the classroom."
+    }
 
+  }
+}
+enum Subject {
+    case math,
+        science,
+        english
+}
 
+protocol Teach {
+   func teachSubject(_ subject:Subject) -> String
+}
 
-
-
-
-
+extension Teach {
+  func teachSubject(_ subject:Subject) -> String {
+    switch  subject{
+    case .math:
+      return "Take out your math books please."
+    case .english:
+      return  "To read or not to read. Everyone take out your english books."
+    case .science:
+      return "Time to learn the best subject of all! Science!!"
+    }
+  }
+}
+protocol Employee: Payable, TimeOff { }
